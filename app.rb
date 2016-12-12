43e2e5e128840e8fcb7d13298494d4dc56e06881
D.P. Hurley <hurley.dannyp@gmail.com>
2016-12-12 00:00:27 -0500

    Renamed data.json to favorites.json. More descriptive, in a database, this would have a descriptive table name.

--- app.rb
+++ app.rb
@@ -4,18 +4,18 @@ get '/' do
   File.read('views/index.html')
 end
 
-get 'favorites' do
+get '/favorites' do
   response.header['Content-Type'] = 'application/json'
-  File.read('data.json')
+  File.read('favorites.json')
 end
 
 get '/favorites' do
-  file = JSON.parse(File.read('data.json'))
+  file = JSON.parse(File.read('favorites.json'))
   
   return 'Invalid Request' unless params[:name] && params[:oid]
 
   movie = { name: params[:name], oid: params[:oid] }
   file << movie
-  File.write('data.json',JSON.pretty_generate(file))
+  File.write('favorites.json',JSON.pretty_generate(file))
   movie.to_json
 end

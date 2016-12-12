341533489d68532f48d9f33dcea5773034fd9f00
D.P. Hurley <hurley.dannyp@gmail.com>
2016-12-12 00:04:22 -0500

    Cleaned up GET /favorites endpoint to serve up placeholder JSON from favorites.json

--- app.rb
+++ app.rb
@@ -1,15 +1,18 @@
 require 'sinatra'
 
+# GET call that serves up the HTML page
 get '/' do 
   File.read('views/index.html')
 end
 
+# This is the GET call that will serve up favorites from the 'database'
 get '/favorites' do
   response.header['Content-Type'] = 'application/json'
   File.read('favorites.json')
 end
 
-get '/favorites' do
+# This is the POST call that will add new favorites to the 'database'
+post '/favorites' do
   file = JSON.parse(File.read('favorites.json'))
   
   return 'Invalid Request' unless params[:name] && params[:oid]
